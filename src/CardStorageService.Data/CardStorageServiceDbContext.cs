07c98282aa42169227412769ec0de67039215011
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2022-09-19 16:35:00 +0500

    add dbsets with account & sessions

--- src/CardStorageService.Data/CardStorageServiceDbContext.cs
+++ src/CardStorageService.Data/CardStorageServiceDbContext.cs
@@ -10,4 +10,6 @@ public class CardStorageServiceDbContext : DbContext
 
     public virtual DbSet<Client> Clients { get; set; }
     public virtual DbSet<Card> Cards { get; set; }
+    public virtual DbSet<Account> Accounts { get; set; }
+    public virtual DbSet<AccountSession> AccountSessions { get; set; }
 }
